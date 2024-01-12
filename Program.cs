cebd06966878a348d6bb8f076cbe8ebc839096bb
Jack Edge <js77js@yahoo.co.uk>
2024-01-12 21:40:39 +0000

    progressing

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@
 // cw-> System.Console.WriteLine();
 // wl-> Console.WriteLine(); (my own snippet)
 
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Reflection.Metadata;
-
 namespace Program
 {
     class Prog
@@ -849,13 +845,29 @@ namespace Program
                         totalAA += numberAA;
                     }
                     // treat as string
-                    else 
+                    else
                     {
                         messageAA += valueAA;
                     }
                 }
                 Console.WriteLine($"Message: {messageAA}");
                 Console.WriteLine($"Total: {totalAA}");
+                Console.WriteLine("");
+
+                // output math operations as specific number types
+                Console.WriteLine("Output math operations as specific number types:");
+                int value1 = 12;
+                decimal value2 = 6.2m;
+                float value3 = 4.3f;
+                // The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+                // Convert.ToInt32() rounds up the way you would expect.
+                int result1 = Convert.ToInt32((decimal)value1 / value2);
+                Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+                decimal result2 = value2 / (decimal)value3;
+                Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+                float result3 = value3 / value1;
+                Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
             }
         }
     }
