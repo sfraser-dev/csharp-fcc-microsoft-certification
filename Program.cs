9bf24f36856d81af4b228c387eddbcfd91095c82
Jack Edge <js77js@yahoo.co.uk>
2024-02-21 22:38:34 +0000

    more error handling (checked)

--- Program.cs
+++ Program.cs
@@ -1955,8 +1955,134 @@ internal class Program
         {
             return "Hi, my name is Andrew. Good to meet you.";
         }
+        Console.WriteLine("");
 
-        
+        Console.WriteLine("############################ Exceptions / Errors\n");
+        // divide by zero
+        double float1 = 3000.0;
+        double float2 = 0.0;
+        int number1 = 3000;
+        int number2 = 0;
+        try
+        {
+            Console.WriteLine(float1 / float2);
+            Console.WriteLine(number1 / number2);
+            Console.WriteLine("Exit program");
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Do not divide by 0" + e);
+        }
 
+        // exception within function propagating
+        try
+        {
+            Process1();
+        }
+        catch
+        {
+            Console.WriteLine("An exception has occurred");
+        }
+
+        Console.WriteLine("Exit program");
+
+        static void Process1()
+        {
+            try
+            {
+                WriteMessage();
+            }
+            catch
+            {
+                Console.WriteLine("Exception caught in Process1");
+            }
+
+        }
+
+        static void WriteMessage()
+        {
+            double float1 = 3000.0;
+            double float2 = 0.0;
+            int number1 = 3000;
+            int number2 = 0;
+
+            Console.WriteLine(float1 / float2);
+            Console.WriteLine(number1 / number2);
+        }
+
+        // inputValues is used to store numeric values entered by a user
+        string[] inputValues = ["three", "9999999999", "0", "2"];
+        foreach (string inputValue in inputValues)
+        {
+            int numValueException = 0;
+            try
+            {
+                numValueException = int.Parse(inputValue);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid readResult. Please enter a valid number.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number you entered is too large or too small.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // catching specific exceptions
+        checked
+        {
+            // checked blocks specically check for overflow exceptions
+            try
+            {
+                int num1 = int.MaxValue;
+                int num2 = int.MaxValue;
+                int resultException = num1 + num2;
+                Console.WriteLine("resultException: " + resultException);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: The number is too large to be represented as an integer. " + ex.Message);
+            }
+        }
+        try
+        {
+            string? str = null;
+            int length = str.Length;
+            Console.WriteLine("String Length: " + length);
+        }
+        catch (NullReferenceException ex)
+        {
+            Console.WriteLine("Error: The reference is null. " + ex.Message);
+        }
+
+        try
+        {
+            int[] numbersException = new int[5];
+            numbersException[5] = 10;
+            Console.WriteLine("Number at index 5: " + numbersException[5]);
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Error: Index out of range. " + ex.Message);
+        }
+
+        try
+        {
+            int num3 = 10;
+            int num4 = 0;
+            int resultException2 = num3 / num4;
+            Console.WriteLine("resultException: " + resultException2);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: Cannot divide by zero. " + ex.Message);
+        }
+
+        Console.WriteLine("Exiting program.");
     }
 }
